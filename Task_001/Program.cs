263a85cd70a9bbce7e0fe4a9adc59a7ab8240ed8
Konstantin <Bunaevkonstantin@bk.ru>
2023-04-08 14:15:23 +0400

    Second stage - main code

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int length = ImputNumber("Введите размер исходного массива", "Введено неверное значение");
+int maxSim = ImputNumber("Введите максимальное число символов в элемете результирующего массива", "Введено неверное значение");
+string[] array = GetArray(length);
+Console.Write("Исходный массив: ");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int lengthResult = GetSizeArray(array, maxSim);
+string[] arrayResult = GetArray2(lengthResult, array, maxSim);
+Console.Write("Результирующий массив: ");
+Console.WriteLine($"[{String.Join(", ", arrayResult)}]");
